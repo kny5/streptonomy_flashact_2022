b4df0e08a7fea7fe169abcd311269d8a75d4027a
Antonio Anaya <anaya.4589@gmail.com>
2022-07-02 02:47:06 -0500

    added start welcome menu, missing clicking functions

--- scenario.py
+++ scenario.py
@@ -9,6 +9,7 @@ from chapin_engine.controller import controller_scroll, controller_angle
 
 #Window setup
 scenario_scale = 1
+pygame.init()
 game_display = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 game_display_window = pygame.display.get_surface().get_size()
 
@@ -19,17 +20,41 @@ labyrinth_img = pygame.image.load("assets/art/fondo_collision.png")
 labyrinth_img_scaled = pygame.transform.scale(labyrinth_img,labyrinth_img.get_size())
 main_character = [pygame.image.load("assets/art/bacteria/avanzar/avanzar/loop_avanzar_" + str(num) + ".png") for num in range(0,24)]
 
+#load start menu assets
+welcome_bg_img = pygame.image.load("assets/inicio/fondo-1080.png")
+welcome_logos_img = pygame.image.load("assets/inicio/conjunto-1080.png")
+welcome_title_img = pygame.image.load("assets/inicio/logo_inicio_00000.png")
+
+
 print(game_display_size, game_display_window)
 
 #Global variables
 def absolute_position():
     pass
+clock = pygame.time.Clock()
 
+#Splash and welcome loop
+game_FPS = 60
+welcoming = True
+while welcoming:
+    clock.tick(game_FPS)
+    game_display.blit(welcome_bg_img, (0,0))
+    game_display.blit(welcome_logos_img, (0,0))
+    game_display.blit(welcome_title_img, (0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RETURN:
+                welcoming = False
+                break
+    pygame.display.update()
 
 #play video
 video_FPS = 20
 video = cv2.VideoCapture('assets/video/intro_master_texto.mp4')
-clock = pygame.time.Clock()
+
 
 play_intro = True
 while play_intro:
@@ -43,7 +68,6 @@ while play_intro:
 
 
 #Game Loop
-game_FPS = 60
 gaming = True
 while gaming:
     clock.tick(game_FPS)
